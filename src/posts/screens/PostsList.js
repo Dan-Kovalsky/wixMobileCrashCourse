b8fda4340756620a2b7e3a75ec2d3b808f369491
Dan Kovalsky <dankov@wix.com>
2019-03-14 16:58:35 +0200

    Step 1 after 6.

--- src/posts/screens/PostsList.js
+++ src/posts/screens/PostsList.js
@@ -1,19 +1,34 @@
-import React, {Component} from 'react';
+import React, {Component, PureComponent} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 import {Navigation} from "react-native-navigation";
 
-class PostsList extends Component {
-
-    static propTypes = {
-        componentId: PropTypes.string
-    };
+class PostsList extends PureComponent {
 
     constructor(props) {
         super(props);
 
         this.pushViewPostScreen = this.pushViewPostScreen.bind(this);
+        Navigation.events().bindComponent(this);
     }
+
+    static get options() {
+        return {
+            topBar: {
+                rightButtons: [
+                    {
+                        id: 'addPost',
+                        text: 'Add'
+                    }
+                ]
+            }
+        };
+    }
+
+    static propTypes = {
+        componentId: PropTypes.string
+
+    };
 
     pushViewPostScreen() {
         Navigation.push(this.props.componentId, {
@@ -31,6 +46,10 @@ class PostsList extends Component {
                 }
             }
         });
+    }
+
+    navigationButtonPressed({buttonId}) {
+        alert(buttonId);
     }
 
 
