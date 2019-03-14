0f5f305c8593eefcb6557b774d8351f603247b5b
Dan Kovalsky <dankov@wix.com>
2019-03-14 15:44:26 +0200

    Step 1 after 3.

--- src/screens.js
+++ src/screens.js
@@ -1,6 +1,18 @@
-import AddPost from './src/posts/screens/AddPost';
-import PostsList from './src/posts/screens/PostList';
+// import { Navigation } from 'react-native-navigation';
+// import AddPost from './src/posts/screens/AddPost';
+// import PostsList from './src/posts/screens/PostList';
+// import ViewPost from './src/posts/screens/ViewPost';
+//
+// export function registerScreens() {
+//     Navigation.registerComponent(`PostsListScreen`, () => PostsList);
+//     Navigation.registerComponent(`AddPostScreen`, () => AddPost);
+//     Navigation.registerComponent(`ViewPostScreen`, () => ViewPost);
+// }
 
+import {Navigation} from 'react-native-navigation';
 
-Navigation.registerComponent(`PostsListScreen`, () => PostsList);
-Navigation.registerComponent(`AddPostScreen`, () => AddPost);
+export function registerScreens() {
+    Navigation.registerComponent('blog.PostsList', () => require('./posts/screens/PostsList').default);
+    Navigation.registerComponent('blog.AddPost', () => require('./posts/screens/AddPost').default);
+    Navigation.registerComponent('blog.ViewPost', () => require('./posts/screens/ViewPost').default);
+}
