2d014d7ddea1a34d0815da1986fd525cdb8047f3
Dan Kovalsky <dankov@wix.com>
2019-03-14 15:56:03 +0200

    Step 1 after 4.

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-/**
- * @format
- */
+import {Navigation} from 'react-native-navigation';
+import {registerScreens} from './src/screens';
 
-import { Navigation } from "react-native-navigation";
-import App from './App';
-
-Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+registerScreens();
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
-      component: {
-        name: "navigation.playground.WelcomeScreen"
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'blog.PostsList',
+              options: {
+                topBar: {
+                  title: {
+                    text: 'Blog'
+                  }
+                }
+              }
+            }
+          }
+        ],
       }
     }
   });
