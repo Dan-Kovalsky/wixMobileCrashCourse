72d4d833fe975e75ef79587afc3241d98ddadb86
Dan Kovalsky <dankov@wix.com>
2019-04-01 17:40:39 +0300

    after adding issues of the last section,not working yet

--- wallaby.js
+++ wallaby.js
@@ -8,7 +8,7 @@ module.exports = function (wallaby) {
         path.join(__dirname, 'node_modules') +
         path.delimiter +
         path.join(__dirname, 'node_modules/react-scripts/node_modules');
-    require('module').Module._initPaths();
+    require('src/module').Module._initPaths();
 
     return {
         files: [
